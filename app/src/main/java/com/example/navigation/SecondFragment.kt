bea9934f203c45d6e61c492a1f79ed5764ec47ad
Mehmet <87245497+mehmetboluk@users.noreply.github.com>
2021-08-11 13:03:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/navigation/SecondFragment.kt
@@ -0,0 +1,48 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_second.*
+
+
+class SecondFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        arguments.let {
+            val user = SecondFragmentArgs.fromBundle(it!!).username
+            tvSecondFragment.text = user
+        }
+
+        btnBack.setOnClickListener{
+
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+
+    }
+
+
+
+}
